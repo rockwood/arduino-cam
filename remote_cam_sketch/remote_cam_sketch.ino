93a699c8b978b00437e6fc8d80113518596f0c50
Kevin Rockwood <kevin@rockwood.me>
2014-05-06 20:07:55 -0400

    Clean up remote-cam sketch

--- remote_cam_sketch/remote_cam_sketch.ino
+++ remote_cam_sketch/remote_cam_sketch.ino
@@ -1,83 +1,65 @@
 
-const int joystickPinClick = 2;      
-const int joystickPinX = A0;
-const int joystickPinY = A1;
-const int ledPin = 13;
+const int JOY_STICK_PIN_X = A0;
+const int JOY_STICK_PIN_Y = A1;
 
-const int left = 11;
-const int right = 3;
-const int up = 6;
-const int down = 9;
+const int PAN_TILT_PIN_UP = 11;
+const int PAN_TILT_PIN_DOWN = 10;
+const int PAN_TILT_PIN_LEFT = 9;
+const int PAN_TILT_PIN_RIGHT = 6;
 
 int responseDelay = 20;
-int range = 510;
-int threshold = 55;
-int center = range/2;
  
-void setup()
-{
+void setup() {
   Serial.begin(115200);
 }
  
 void loop()  {
-  int joystickCurrentX = readAxis(joystickPinX);
-  int joystickCurrentY = readAxis(joystickPinY);
+  int currentJoystickX = readAxis(JOY_STICK_PIN_X);
+  int currentJoystickY = readAxis(JOY_STICK_PIN_Y);
  
-  if (joystickCurrentY > 0)
-  {
-    analogWrite(up, abs(joystickCurrentY));
-    analogWrite(down, 0);
+  if (currentJoystickX > 0){
+    analogWrite(PAN_TILT_PIN_RIGHT, abs(currentJoystickX));
+    analogWrite(PAN_TILT_PIN_LEFT, 0);
   }
- 
-  else if (joystickCurrentY < 0)
-  {
-    analogWrite(down, abs(joystickCurrentY));
-    analogWrite(up, 0);
+  else if (currentJoystickX < 0){
+    analogWrite(PAN_TILT_PIN_LEFT, abs(currentJoystickX));
+    analogWrite(PAN_TILT_PIN_RIGHT, 0);
   }
-  // Stop tilt
-  else
-  {
-    analogWrite(up, 0);
-    analogWrite(down, 0);
-  }
- 
-  // Pan based on the input from the joystick
-  if (joystickCurrentX > 0)
-  {
-    analogWrite(right, abs(joystickCurrentX));
-    analogWrite(left, 0);
-  }
- 
-  else if (joystickCurrentX < 0)
-  {
-    analogWrite(left, abs(joystickCurrentX));
-    analogWrite(right, 0);
-  }
- 
-  else
-  {
-    analogWrite(right, 0);
-    analogWrite(left, 0);
+  else{
+    analogWrite(PAN_TILT_PIN_RIGHT, 0);
+    analogWrite(PAN_TILT_PIN_LEFT, 0);
   }
   
-  Serial.print(joystickCurrentX);
+  if (currentJoystickY > 0) {
+    analogWrite(PAN_TILT_PIN_UP, abs(currentJoystickY));
+    digitalWrite(PAN_TILT_PIN_DOWN, 0);
+  }
+  else if (currentJoystickY < 0) {
+    analogWrite(PAN_TILT_PIN_DOWN, abs(currentJoystickY));
+    analogWrite(PAN_TILT_PIN_UP, 0);
+  }
+  else {
+    analogWrite(PAN_TILT_PIN_UP, 0);
+    analogWrite(PAN_TILT_PIN_DOWN, 0);
+  }
+  
+  Serial.print(currentJoystickX);
   Serial.print(", ");  
-  Serial.print(joystickCurrentY);
+  Serial.print(currentJoystickY);
   Serial.print(", ");  
   Serial.println();
 
   delay(responseDelay);
 }
 
-int readAxis(int pin) { 
-  // read the analog input:
+int readAxis(int pin) {
+  int range = 510;
+  int threshold = 55;
+  int center = range/2;
   int reading = analogRead(pin);
 
-  // map the reading from the analog input range to the output range:
   reading = map(reading, 0, 1023, 0, range);
 
-  // if the output reading is outside from the
-  // rest position threshold,  use it:
   int distance = reading - center;
   
   if (abs(distance) < threshold) {
