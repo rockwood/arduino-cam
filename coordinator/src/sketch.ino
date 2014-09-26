dcac5b1ae61fa31e72064b6bda2a0a7e329ea786
Kevin Rockwood <kevin@rockwood.me>
2014-09-26 13:53:05 -0400

    Add camera library for transmission

--- coordinator/src/sketch.ino
+++ coordinator/src/sketch.ino
@@ -1,18 +1,34 @@
 #include <Joystick.h>
-// #include <Transmiter.h>
+#include <Camera.h>
 
 uint8_t payload[] = { 0, 0, 0 };
 
+int statusLed = 13;
+
 Joystick joystick = Joystick(A0, A1, A2);
+Camera cam1 = Camera(0x13A200, 0x40AFDCA7, payload);
 
 void setup() {
   Serial.begin(9600);
+  cam1.setSerial(Serial);
+  pinMode(statusLed, OUTPUT);
 }
 
-void loop()  {
+void loop() {
   joystick.readInto(payload);
-
-  
-  delay(100);
+  cam1.sendPayload();
+  flashLed(statusLed, 3, payload[0]);
+  delay(1000);  
 }
 
+void flashLed(int pin, int times, int wait) {
+  for (int i = 0; i < times; i++) {
+    digitalWrite(pin, HIGH);
+    delay(wait);
+    digitalWrite(pin, LOW);
+
+    if (i + 1 < times) {
+      delay(wait);
+    }
+  }
+}
